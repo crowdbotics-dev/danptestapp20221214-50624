ebf0535475ab14f6327356e16357af2fc4c6984a
Daniel Palacios <daniel80380@gmail.com>
2022-12-21 11:06:58 -0300

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,46 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+  "hr": {
+    "marginTop": 20,
+    "marginBottom": 20,
+    "borderBottomColor": "black",
+    "borderBottomWidth": 1
+  },
+  "container": {
+    "flex": 1,
+    "height": 100,
+    "padding": 13
+  },
+  "text": {
+    "color": "black",
+    "fontSize": 20
+  },
+  "buttonPressed": {
+    "backgroundColor": "aquamarine"
+  },
+  "buttonNotPressed": {
+    "backgroundColor": "blue"
+  },
+  "button": {
+    "borderRadius": 4,
+    "padding": 15,
+    "marginTop": 10
+  },
+  "buttonText": {
+    "color": "white",
+    "textAlign": "center",
+    "fontSize": 16
+  }
+});
 export const globalOptions = {
-  name: "danptestapp20221214-50624",
-  url: "https://danptestapp20221214-50624.botics.co",
-  api: "https://danptestapp20221214-50624.botics.co/api/v1"
+  "name": "danptestapp20221214-50624",
+  "url": "https://danptestapp20221214-50624.botics.co",
+  "api": "https://danptestapp20221214-50624.botics.co/api/v1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
-    copy: "Routes available!"
+    "title": "App Menu",
+    "copy": "Routes available",
+    "styles": styles
   }
 };
